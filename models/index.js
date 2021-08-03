fe8608bf81fb2982a7b8eb30fe1759f4cceec774
Erin Lim <erinlim@utexas.edu>
2021-08-03 10:05:19 -0500

    added bycrpt

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Cars = require('./cars');
+const Driver = require('./driver');
+const Location = require('./location');
+const Pickup = require('./pickup');
+const Rider = require('./rider');
+
+
+
+module.exports = {
+    Cars,
+    Driver,
+    Location,
+    Pickup,
+    Rider
+}
