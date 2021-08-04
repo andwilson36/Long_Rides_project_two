f392d1c6c3f0e0ce1c4309abfe4ecb73d098e190
Erin Lim <erinlim@utexas.edu>
2021-08-04 10:30:29 -0500

    added to user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -14,6 +14,7 @@ router.post('/',async (req, res) => {
         //set  up sessions with loggedIn variable set to true
         req.session.save(() => {
             req.session.username = newUser.username;
+            req.session.email = newUser.email;
             req.session.loggedIn = true;
             res.status(200).json(newUser);
           });
@@ -24,3 +25,54 @@ router.post('/',async (req, res) => {
 });
 
 //Login
+router.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+  
+      if (!user) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
+      }
+  
+      const validPassword = await user.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
+      }
+      req.session.save(() => {
+        req.session.username = user.username;
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  //logout 
+  router.post('/logout', (req, res) => {
+    // When the user logs out, destroy the session
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
+  
