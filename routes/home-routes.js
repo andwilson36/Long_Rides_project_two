23313a6df75a8cfb57ac1f8af78cfa7f2aeecc8a
Rico Perez <ricoperez1@gmail.com>
2021-08-04 13:52:21 -0500

    added to home routes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -4,12 +4,51 @@ const { User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    // const dbDriverData = await driver.findAll({
-      
-    // });
 
 
     res.render("login-page", {
+      
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/signup", async (req, res) => {
+  try {
+
+
+    res.render("signup-page", {
+      
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/profile", async (req, res) => {
+  try {
+
+
+    res.render("profile-page", {
+      
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/dashboard", async (req, res) => {
+  try {
+
+
+    res.render("dashboard", {
       
       // loggedIn: req.session.loggedIn,
     });
