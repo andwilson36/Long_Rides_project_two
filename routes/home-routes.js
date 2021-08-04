3732a882a627da2154e4aaf1f9bda40520efbb4f
Erin Lim <erinlim@utexas.edu>
2021-08-04 14:44:44 -0500

    fixed deleted homeroutes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -18,4 +18,44 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
+router.get("/signup", async (req, res) => {
+  try {
+
+
+    res.render("signup-page", {
+
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+router.get("/profile", async (req, res) => {
+  try {
+
+
+    res.render("profile-page", {
+
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+router.get("/dashboard", async (req, res) => {
+  try {
+
+
+    res.render("dashboard", {
+
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
