9351125a0cbeb3d4558edec86dd74f7cc95e7907
Erin Lim <erinlim@utexas.edu>
2021-08-04 14:38:52 -0500

    added auth

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,55 +2,15 @@ const router = require("express").Router();
 const withAuth = require("../utils/auth");
 const { User } = require("../models");
 
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
+    const userData = await User.findAll();
 
+    const users = userData.map((project) => project.get({ plain: true }));
 
     res.render("login-page", {
-      
-      // loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get("/signup", async (req, res) => {
-  try {
-
-
-    res.render("signup-page", {
-      
-      // loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get("/profile", async (req, res) => {
-  try {
-
-
-    res.render("profile-page", {
-      
-      // loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get("/dashboard", async (req, res) => {
-  try {
-
-
-    res.render("dashboard", {
-      
-      // loggedIn: req.session.loggedIn,
+      users,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     console.log(err);
--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
     if (!req.session.loggedIn) {
-      res.redirect('/login');
+      res.render('login-page');
     } else {
       next();
     }
