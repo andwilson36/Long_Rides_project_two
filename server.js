0e452140836eb1a7e625c8399e4a6e9f5c301adb
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-05 10:37:47 -0500

    fixxed css and js link

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use("/static", express.static('./static/'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
