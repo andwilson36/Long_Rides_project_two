515e714a3158f0f94b1a839db3309658adc902e1
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-05 11:37:31 -0500

    fixed links with css and js:

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-// app.use("/static", express.static('static'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
