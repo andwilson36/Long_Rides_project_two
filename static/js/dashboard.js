8c00ef0007ee45bb944457334d7b6d6aad6b33f8
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-07 21:56:08 -0500

    dashboard js

---
+++ static/js/dashboard.js
@@ -0,0 +1,6 @@
+function setValue(pickup, destination) {
+    let pickupInput = document.getElementById('#start');
+    pickupInput.value = pickup;
+    let destinationInput = document.getElementById('#end');
+    destinationInput.value = destination;
+  }
