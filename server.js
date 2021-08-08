8bbfa3bcabf23ebded3d8b8935c3330067098ce0
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-08 14:56:52 -0500

    fixed buttons on dashboard

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 86400,
+    maxAge: 8640000,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,6 +1,38 @@
 function setValue(pickup, destination) {
-    let pickupInput = document.getElementById('#start');
-    pickupInput.value = pickup;
-    let destinationInput = document.getElementById('#end');
-    destinationInput.value = destination;
+  let startingAddress;
+  let endingAddress;
+  console.log(destination)
+  switch (pickup) {
+    case 'UT Campus':
+      startingAddress = 'University of Texas, Austin, TX';
+      break;
+    case 'Austin Airport':
+      startingAddress = '3600 Presidential Blvd, Austin, TX 78719';
+      break;
+    case 'Rainey Street':
+      startingAddress = '2207 Lou Neff Rd, Austin, TX 78746';
+      break;
+    case 'Barton Springs Pool':
+      startingAddress = 'Barton Springs Rd, Austin, TX 78704'
   }
+  switch (destination) {
+    case 'UT Campus':
+      endingAddress = 'University of Texas, Austin, TX';
+      break;
+    case 'Austin Airport':
+      endingAddress = '3600 Presidential Blvd, Austin, TX 78719';
+      break;
+    case 'Rainey Street':
+      endingAddress = '2207 Lou Neff Rd, Austin, TX 78746';
+      break;
+    case 'Zilker Park':
+      endingAddress = '2207 Lou Neff Rd, Austin, TX 78746'
+      break;
+    case 'Barton Springs Pool':
+      endingAddress = 'Barton Springs Rd, Austin, TX 78704'
+      break;
+  }
+
+  document.getElementById('start').value = startingAddress;
+  document.getElementById('end').value = endingAddress;
+}
